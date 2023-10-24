7f4986818c14fcd48004508f2b894d5c92640e25
Dan <daniel.stasiuk@1worldonline.com>
2023-10-24 23:12:22 +0300

    add update user logic

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -7,11 +7,9 @@ export const test = (req, res) => {
     message: "Hola",
   });
 };
-
 export const updateUser = async (req, res, next) => {
   if (req.user.id !== req.params.id)
-    return next(errorHandler(401, "you can only update your own account"));
-
+    return next(errorHandler(401, "You can only update your own account"));
   try {
     if (req.body.password) {
       req.body.password = bcryptjs.hashSync(req.body.password, 10);
@@ -32,9 +30,7 @@ export const updateUser = async (req, res, next) => {
 
     const { password, ...rest } = updatedUser._doc;
 
-    res.status(200).json({
-      rest,
-    });
+    res.status(200).json(rest);
   } catch (error) {
     next(error);
   }
