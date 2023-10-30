624ce6a659d9a99959ec920cdee1bdcf604e738e
Dan <daniel.stasiuk@1worldonline.com>
2023-10-30 22:33:42 +0200

    add search api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -36,7 +36,7 @@ export const updateListing = async (req, res, next) => {
     return next(errorHandler(401, "You can only delete your own listings"));
 
   try {
-    const updatedlisting = await Listing.findByIdAndUpdate(
+    const updatedListing = await Listing.findByIdAndUpdate(
       req.params.id,
       req.body,
       {
@@ -44,7 +44,7 @@ export const updateListing = async (req, res, next) => {
       }
     );
 
-    res.status(200).json(updatedlisting);
+    res.status(200).json(updatedListing);
   } catch (error) {
     next(error);
   }
@@ -57,6 +57,57 @@ export const getListing = async (req, res, next) => {
     if (!listing) return next(errorHandler(404, "Listing not found"));
 
     res.status(200).json(listing);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getListings = async (req, res, next) => {
+  try {
+    const limit = parseInt(req.query.limit) || 10;
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    let offer = req.query.offer;
+
+    if (offer === undefined || offer === "false") {
+      offer = { $in: [false, true] };
+    }
+
+    let furnished = req.query.furnished;
+
+    if (furnished === undefined || furnished === "false") {
+      furnished = { $in: [false, true] };
+    }
+
+    let parking = req.query.parking;
+
+    if (parking === undefined || parking === "false") {
+      parking = { $in: [false, true] };
+    }
+
+    let type = req.query.type;
+
+    if (type === undefined || type === "all") {
+      type = { $in: ["rent", "sale"] };
+    }
+
+    const searchTerm = req.query.searchTerm || "";
+
+    const sort = req.query.sort || "createdAt";
+
+    const order = req.query.order || "desc";
+
+    const listings = await Listing.find({
+      name: { $regex: searchTerm, $options: "i" },
+      offer,
+      furnished,
+      parking,
+      type,
+    })
+      .sort({ [sort]: order })
+      .limit(limit)
+      .skip(startIndex);
+
+    return res.status(200).json(listings);
   } catch (error) {
     next(error);
   }
